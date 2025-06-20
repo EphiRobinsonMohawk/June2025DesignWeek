2d8afffe8aea0735fca35889c22105a1d582840c
Ephi Robinson <000969115@mohawkcollege.ca>
2025-06-20 00:55:07 -0400

    music button on m

--- JET-DesignWeek/Assets/Scripts/MusicMan.cs
+++ JET-DesignWeek/Assets/Scripts/MusicMan.cs
@@ -8,6 +8,7 @@ public class MusicMan : MonoBehaviour
     public static MusicMan instance;
     public AudioSource mAudioSource;
     public AudioClip mainTheme;
+    public bool isPlaying;
     void Awake()
     {
         if (instance == null)
@@ -19,7 +20,27 @@ public class MusicMan : MonoBehaviour
         {
             Destroy(gameObject); // Destroy duplicates
         }
+        isPlaying = true;
         mAudioSource.Play();
 
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            //Can reference the scene by name in quotes (string)
+            //or by the index number (0, 1, 2, etc.)
+            if (isPlaying)
+            {
+
+                mAudioSource.Stop();
+                isPlaying = false;
+            }
+            else if (isPlaying == false)
+            {
+                mAudioSource.Play();
+                isPlaying = true;
+            }
+        }
     }
 }
